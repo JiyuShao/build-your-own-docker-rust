9eddfd32e6623f87a21be49e54271effbbf28326
Jiyu Shao <jiyu.shao@gmail.com>
2024-04-29 10:35:05 +0800

    add docker cache clean logic

--- test_your_docker.sh
+++ test_your_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker container prune -f
+docker system prune --force
 docker build -t docker-tester-dev . && docker run --cap-add "SYS_ADMIN" -e "TERM=xterm-256color" docker-tester-dev make test
